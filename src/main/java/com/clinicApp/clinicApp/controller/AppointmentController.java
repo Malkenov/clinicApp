e8ae27bd2faa8106ae98ed4e401d33391c11a22f
asanali_malkenov22@icloud.com <asanali_1997@mail.ru>
2025-11-08 00:10:20 +0500

    двадцать первый коммит

--- src/main/java/com/clinicApp/clinicApp/controller/AppointmentController.java
+++ src/main/java/com/clinicApp/clinicApp/controller/AppointmentController.java
@@ -1,9 +1,14 @@
 package com.clinicApp.clinicApp.controller;
 
+import com.clinicApp.clinicApp.dto.request.AppointmentRequestDto;
+import com.clinicApp.clinicApp.dto.response.AppointmentResponseDto;
 import com.clinicApp.clinicApp.service.AppointmentService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @AllArgsConstructor
 @RestController
@@ -12,5 +17,22 @@ public class AppointmentController {
 
     private final AppointmentService appointmentService;
 
+    @GetMapping
+    public ResponseEntity<List<AppointmentResponseDto>> postAppointment(){
+        List<AppointmentResponseDto> dto = appointmentService.getAll();
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AppointmentResponseDto> getById(@PathVariable Long id){
+        return ResponseEntity.ok(appointmentService.getById(id));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<AppointmentResponseDto> updateAppointment(
+            @RequestBody @Validated AppointmentRequestDto dto,
+            @PathVariable Long id){
+        return ResponseEntity.ok(appointmentService.update(dto,id));
+    }
 
 }
